8d228d4a119964fa5a62dbcbe9810edebbe212bd
Sławomir Pacyno <wilczogon@gmail.com>
2015-03-19 14:31:27 +0100

    some changes

--- C/tests/isend.c
+++ C/tests/isend.c
@@ -8,6 +8,7 @@ int main(int argc, char** argv) {
   MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
   int world_size;
   MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+  MPI_Status status;
 
   // We are assuming at least 2 processes for this task
   if (world_size < 2) {
@@ -18,15 +19,17 @@ int main(int argc, char** argv) {
   if (world_rank == 0) {
     // If we are rank 0, set the number to -1 and send it to process 1
     number = -1;
-    MPI_Isend(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+    MPI_Request request;
+    MPI_Isend(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD, &request);
+    MPI_Wait(&request, &status);
+    MPI_Finalize();
   } else if (world_rank == 1) {
+
+    MPI_Request request;
     
-    int res; 
-    
-    do{
-      res = MPI_Irecv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-      sleep(10);
-    }while(res != MPI_SUCCESS );
+    MPI_Irecv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &request);
+    MPI_Wait(&request, &status);
+    MPI_Finalize();
     
     printf("Process 1 received number %d from process 0\n", number);
   }
