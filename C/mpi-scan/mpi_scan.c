06f592530afd1e0598a710da940aacef9e3add91
Wojcik Jan <jawojcik@vnode-01.dydaktyka.icsr.agh.edu.pl>
2015-03-25 22:46:13 +0100

    Began working on Mpi_scan

---
+++ C/mpi-scan/mpi_scan.c
@@ -0,0 +1,36 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Program performs simple example of collective communication:
+ * For each process, 
+ * scan function computes the product of rank of all processes increased
+ * by one (each rank)
+ * up to that process (inclusive).
+ * Every process prints down its number, the result and elapsed time.
+ */
+int main(int argc, char** argv) {
+  MPI_Init(NULL, NULL);
+  int world_rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+  int world_size;
+  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+  // We are assuming at least 2 processes for this task
+  if (world_size < 2) {
+    fprintf(stderr, "World size must be greater than 1 for %s\n", argv[0]);
+    MPI_Abort(MPI_COMM_WORLD, 1); 
+  }
+	int numbers,i;
+	double time=MPI_Wtick();	
+	int number=world_rank+1,result;
+	//loading the operation by repeating the scan many times
+	for(i=0;i<150000;i++) {
+		MPI_Scan(&number,&result,1,MPI_INT,MPI_PROD,MPI_COMM_WORLD);
+	}
+	time=MPI_Wtick()-time;
+  	MPI_Finalize();
+	if(world_rank==world_size-1) {
+		printf("Result: %d. Elapsed time: %f",result,time);
+	}
+}
